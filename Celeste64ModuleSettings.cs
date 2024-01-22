c1163b8ce6c9cf515cf8a1de1a2e4bd4ed38fd7a
baltdev <heptor42@gmail.com>
2024-01-21 21:02:42 -0600

    Build

--- Celeste64ModuleSettings.cs
+++ Celeste64ModuleSettings.cs
@@ -13,21 +13,19 @@ namespace Celeste.Mod.Celeste64 {
         public bool EnableSM64 { get; set; } = false;
 
         public void CreateEnableSM64Entry(TextMenu menu, bool inGame) {
-            if (!inGame) {
-                var enabler = new 
-                    TextMenu.OnOff("Enable SM64 movement", false)
-                    .Change(value => {
-                        EnableSM64 = Celeste64Module.initializedRom && value;
-                    });
-                enabler.Disabled = !Celeste64Module.initializedRom;
-                menu.Add(enabler);
-                if (!Celeste64Module.initializedRom) {
-                    var message = new TextMenuExt.SubHeaderExt("Failed to initialize libSM64.\nCheck that the rom path is valid and restart.") {
-                        TextColor = Color.OrangeRed,
-                        HeightExtra = 0f
-                    };
-                    menu.Add(message);
-                }
+            var enabler = new 
+                TextMenu.OnOff("Enable SM64 movement", false)
+                .Change(value => {
+                    EnableSM64 = Celeste64Module.initializedRom && value;
+                });
+            enabler.Disabled = !Celeste64Module.initializedRom;
+            menu.Add(enabler);
+            if (!Celeste64Module.initializedRom) {
+                var message = new TextMenuExt.SubHeaderExt("Failed to initialize libSM64.\nCheck that the rom path is valid and restart.") {
+                    TextColor = Color.OrangeRed,
+                    HeightExtra = 0f
+                };
+                menu.Add(message);
             }
         }
     }
